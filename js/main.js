6cc50fd94436a76ef74afa020b12d63453fb8705
VenturaGiu <giulia-ratti@hotmail.com>
2021-06-23 17:36:06 -0300

    Login 100% Funcionante

--- js/main.js
+++ js/main.js
@@ -1,76 +1,202 @@
-//PUXAR LISTA COM TODOS OS USUÁRIOS
-var app = angular.module('myApp', ['ngResource', 'ngRoute']);
+let app = angular.module('myApp', ['ngResource', 'ngRoute', 'ngCookies']);
 
 app.config(['$routeProvider',
-function($routeProvider) {
-  $routeProvider
-  .when('/', {
-    templateUrl: 'listar.html',
-    controller: 'Usuario',
-  })
-  .when('/cadastrar', {
-    templateUrl: 'cadastrar.html',
-    controller: 'Usuario',
-  })
-  .when('/editar/:id', {
-    templateUrl: 'editar.html',
-    controller: 'Usuario',
-  })
-  .otherwise({
-    redirectTo: '/'
-  });
-}
+  function ($routeProvider) {
+    $routeProvider
+      .when('/login', {
+        templateUrl: 'listar.html',
+        controller: 'Usuario',
+      })
+      .when('/cadastrar', {
+        templateUrl: 'cadastrar.html',
+        controller: 'Usuario',
+      })
+      .when('/editar/:id', {
+        templateUrl: 'editar.html',
+        controller: 'Usuario',
+      })
+      .when('/', {
+        templateUrl: './login.html',
+        controller: 'Usuario',
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }
 ])
 
-app.factory('User', ['$resource', function($resource) {
-  return $resource('http://localhost:3001/users/:id?', {id: '@id'}, {
-    get: {method: 'GET'},
-    post: {method: 'POST'},
-    put: {method: 'PUT'},
-    delete: {method: 'DELETE'},
-  });
+app.config(['$httpProvider', function($httpProvider) {
+  $httpProvider.interceptors.push('myHttpInterceptor');
+  $httpProvider.interceptors.push('TokenInterceptor');
 }]);
 
-app.factory('Teste', ['$resource', function($resource) {
-  return $resource('http://localhost:3001/users/listAll/:id?', {id: '@id'}, {
-    getA: {method: 'GET', isArray: true},
-  });
+app.factory('TokenInterceptor', ['$q',
+  '$cookies',
+  function ($q, $cookies) {
+    return {
+      request: function (config) {
+        config.headers = config.headers || {};
+        if ($cookies.get('bolinho')) {
+          config.headers.Authorization = $cookies.get('bolinho').trim();
+        }
+        return config;
+      },
+      requestError: function (rejection) {
+        window.location.href = '#!/';
+        return $q.reject(rejection);
+      },
+      /*
+          * Set Authentication.isAuthenticated to true if 200
+          * received
+          */
+      response: function (response) {
+        return response || $q.when(response);
+      },
+      /*
+          * Revoke client authentication if 401 is received
+          */
+      responseError: function (rejection) {
+        if (rejection != null && rejection.status === 401) {
+          window.location.href = '#!/';
+          $cookies.remove('bolinho');
+        }
+
+        return $q.reject(rejection);
+      }
+    };
+  }
+]);
+
+app.factory('myHttpInterceptor', ['$q', '$rootScope', '$timeout',
+  function ($q, $rootScope, $timeout) {
+    return {
+      request: function (config) {
+        console.info('requisição iniciada ' + new Date())
+        return config;
+      },
+      response: function (response) {
+        console.info('requisição finalizada ' + new Date())
+        var msg = response && response.data && response.data.customMsg ? response.data.customMsg :
+          response && response.data && response.data.msg ? response.data.msg : undefined;
+        if (msg) { 
+          console.log(msg + ' msg vinda da api')
+        }
+
+        return response;
+      },
+      responseError: function (response) {
+        console.info('requisição com falha finalizada ' + new Date())
+        if (response && response.status == 401) {
+          window.location.href = '#!/';
+          return $q.reject(response);
+        }
+
+        var msg = response && response.data && response.data.customMsg ? response.data.customMsg :
+          response && response.data && response.data.msg ? response.data.msg : undefined;
+
+        var customMsg = response && response.data && response.data.customMsg;
+
+        if (response && response.status == 403 || response.status == 401) {
+          msg = customMsg || 'Você não está autorizado a executar esta solicitação';
+          console.error(msg)
+          window.location.href = '#!/';
+          return $q.reject(response);
+        }
+        if (response && response.status == 404) {
+          msg = 'Pagina nao encontrada ou Recurso Inacessivel. Confira sua url ou tente novamente mais tarde.'
+        } else if (response && response.status == 400 &&
+          (response.data && (response.data[0] && response.data[0].message || response.data.msg))) {
+          // msg = customMsg || response && response.data && response.data[0] && response.data[0].message ? response.data[0].message : response && response.data && response.data.msg ? response.data.msg : 'Dados invalidos';
+          window.location.href = '#!/';
+        } else {
+          msg = customMsg ? customMsg : msg ? msg : 'Nao foi possivel executar a solicitacao. Contacte o Administrator e tente novamente mais tarde'
+          console.error(msg)
+          window.location.href = '#!/';
+          return $q.reject(response);
+        }
+        window.location.href = '#!/';
+        console.error(msg);
+        return $q.reject(response);
+      }
+    };
+  }
+]);
+
+app.factory('User', ['$resource', '$cookies', function ($resource) {
+  return $resource('http://localhost:3001/users/:id?', { id: '@id' }, {
+  get: { method: 'GET' },
+  post: { method: 'POST' },
+  put: { method: 'PUT' },
+  delete: { method: 'DELETE' },
+});
 }]);
 
-app.controller('Usuario', function ($scope, User, Teste, $routeParams) {
-  Teste.getA().$promise.then(function(data){
+app.factory('LoginPost', ['$resource', '$cookies', function ($resource) {
+  return $resource('http://localhost:3001/users/login', { id: '@id' }, {
+  post: { method: 'POST' },
+});
+}]);
+
+app.factory('Teste', ['$resource', '$cookies', function ($resource) {
+  return $resource('http://localhost:3001/users/listAll/:id?', { id: '@id' }, {
+  getA: { method: 'GET', isArray: true },
+});
+}]);
+
+app.controller('Usuario', function ($scope, User, Teste, LoginPost, $routeParams, $cookies) {
+  Teste.getA().$promise.then(function (data) {
     $scope.usuario = data;
   })
   
-  $scope.delete = function(id){
-    User.delete({id:id}).$promise.then(function(data){
+  $scope.delete = function (id) {
+    User.delete({ id: id }).$promise.then(function (data) {
       alert('usuario removido')
-      location.reload(); 
-    }).catch(function(data){
+      location.reload();
+    }).catch(function (data) {
       alert('usuario não removido')
     })
   }
   
-  if ($routeParams.id){
-    User.get({id: $routeParams.id}).$promise.then(function(data){
+  if ($routeParams.id) {
+    User.get({ id: $routeParams.id }).$promise.then(function (data) {
       $scope.usuario = data;
     })
   }
   
-  $scope.saveData = function(){
+  $scope.saveData = function () {
     let obj = {
-      name: $scope.name, 
+      name: $scope.name,
       email: $scope.email,
       password: $scope.password
     }
-    User.post(obj).$promise.then(function(usuariosalvo){
+    User.post(obj).$promise.then(function (usuariosalvo) {
       console.log(usuariosalvo);
     })
   }
   
-  $scope.postdata = function(){
-    User.put({id:$scope.usuario._id}, $scope.usuario).$promise.then(function(usuariosalvo){
+  $scope.postdata = function () {
+    User.put({ id: $scope.usuario._id }, $scope.usuario).$promise.then(function (usuariosalvo) {
       console.log(usuariosalvo);
     })
+  }
+  
+  $scope.login = function () {
+    let obj = {
+      email: $scope.email,
+      password: $scope.password
+    }
+    LoginPost.post(obj).$promise.then(function (data) {
+      if (data.message) {
+        alert('Login ou senha errado! Tente novamente')
+      } else {
+        let bolinho = $cookies.put('bolinho', data.token)
+        window.location.href = '#!/login';
+      }
+    })
+  }
+
+  $scope.removeCookie = function () {
+    $cookies.remove('bolinho');
+    window.location.href = '#!/';
   }
 });
