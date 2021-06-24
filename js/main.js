18dcd4220b732cc069730aa401ffe9213a17682d
VenturaGiu <giulia-ratti@hotmail.com>
2021-06-24 17:09:55 -0300

    Sistema de Permissoes completo

--- js/main.js
+++ js/main.js
@@ -5,19 +5,19 @@ app.config(['$routeProvider',
     $routeProvider
       .when('/login', {
         templateUrl: 'listar.html',
-        controller: 'Usuario',
+        controller: 'Usuario as vm',
       })
       .when('/cadastrar', {
         templateUrl: 'cadastrar.html',
-        controller: 'Usuario',
+        controller: 'Usuario as vm',
       })
       .when('/editar/:id', {
         templateUrl: 'editar.html',
-        controller: 'Usuario',
+        controller: 'Usuario as vm',
       })
       .when('/', {
         templateUrl: './login.html',
-        controller: 'Usuario',
+        controller: 'Usuario as vm',
       })
       .otherwise({
         redirectTo: '/'
@@ -25,7 +25,7 @@ app.config(['$routeProvider',
   }
 ])
 
-app.config(['$httpProvider', function($httpProvider) {
+app.config(['$httpProvider', function ($httpProvider) {
   $httpProvider.interceptors.push('myHttpInterceptor');
   $httpProvider.interceptors.push('TokenInterceptor');
 }]);
@@ -46,15 +46,15 @@ app.factory('TokenInterceptor', ['$q',
         return $q.reject(rejection);
       },
       /*
-          * Set Authentication.isAuthenticated to true if 200
-          * received
-          */
+      * Set Authentication.isAuthenticated to true if 200
+      * received
+      */
       response: function (response) {
         return response || $q.when(response);
       },
       /*
-          * Revoke client authentication if 401 is received
-          */
+      * Revoke client authentication if 401 is received
+      */
       responseError: function (rejection) {
         if (rejection != null && rejection.status === 401) {
           window.location.href = '#!/';
@@ -78,7 +78,7 @@ app.factory('myHttpInterceptor', ['$q', '$rootScope', '$timeout',
         console.info('requisição finalizada ' + new Date())
         var msg = response && response.data && response.data.customMsg ? response.data.customMsg :
           response && response.data && response.data.msg ? response.data.msg : undefined;
-        if (msg) { 
+        if (msg) {
           console.log(msg + ' msg vinda da api')
         }
 
@@ -124,30 +124,48 @@ app.factory('myHttpInterceptor', ['$q', '$rootScope', '$timeout',
 
 app.factory('User', ['$resource', '$cookies', function ($resource) {
   return $resource('http://localhost:3001/users/:id?', { id: '@id' }, {
-  get: { method: 'GET' },
-  post: { method: 'POST' },
-  put: { method: 'PUT' },
-  delete: { method: 'DELETE' },
-});
+    get: { method: 'GET' },
+    post: { method: 'POST' },
+    put: { method: 'PUT' },
+    delete: { method: 'DELETE' },
+  });
 }]);
 
 app.factory('LoginPost', ['$resource', '$cookies', function ($resource) {
   return $resource('http://localhost:3001/users/login', { id: '@id' }, {
-  post: { method: 'POST' },
-});
+    post: { method: 'POST' },
+  });
+}]);
+
+app.factory('emailGet', ['$resource', '$cookies', function ($resource) {
+  return $resource('http://localhost:3001/users/email/:email', { email: '@email' }, {
+    get: { method: 'GET' },
+  });
 }]);
 
 app.factory('Teste', ['$resource', '$cookies', function ($resource) {
-  return $resource('http://localhost:3001/users/listAll/:id?', { id: '@id' }, {
-  getA: { method: 'GET', isArray: true },
-});
+  return $resource('http://localhost:3001/users/listAll', {}, {
+    getA: { method: 'GET', isArray: true },
+  });
 }]);
 
-app.controller('Usuario', function ($scope, User, Teste, LoginPost, $routeParams, $cookies) {
-  Teste.getA().$promise.then(function (data) {
-    $scope.usuario = data;
-  })
-  
+app.service('serviceAdmin', function ($cookies) {
+  this.isAdmin = function () {
+    return $cookies.get('admin') == "true";
+  }
+})
+
+app.controller('Usuario', function ($scope, User, Teste, LoginPost, emailGet, $routeParams, $cookies, serviceAdmin, $location) {
+  var vm = this;
+
+  $scope.statusAdmin = serviceAdmin.isAdmin();
+  console.log($cookies.get('bolinho'), $location.url())
+  if ($cookies.get('bolinho')) {
+    Teste.getA().$promise.then(function (data) {
+      $scope.usuario = data;
+    })
+  }
+
   $scope.delete = function (id) {
     User.delete({ id: id }).$promise.then(function (data) {
       alert('usuario removido')
@@ -156,30 +174,31 @@ app.controller('Usuario', function ($scope, User, Teste, LoginPost, $routeParams
       alert('usuario não removido')
     })
   }
-  
+
   if ($routeParams.id) {
     User.get({ id: $routeParams.id }).$promise.then(function (data) {
       $scope.usuario = data;
     })
   }
-  
+
   $scope.saveData = function () {
     let obj = {
       name: $scope.name,
       email: $scope.email,
-      password: $scope.password
+      password: $scope.password,
+      admin: $scope.admin
     }
     User.post(obj).$promise.then(function (usuariosalvo) {
       console.log(usuariosalvo);
     })
   }
-  
+
   $scope.postdata = function () {
     User.put({ id: $scope.usuario._id }, $scope.usuario).$promise.then(function (usuariosalvo) {
       console.log(usuariosalvo);
     })
   }
-  
+
   $scope.login = function () {
     let obj = {
       email: $scope.email,
@@ -189,14 +208,19 @@ app.controller('Usuario', function ($scope, User, Teste, LoginPost, $routeParams
       if (data.message) {
         alert('Login ou senha errado! Tente novamente')
       } else {
-        let bolinho = $cookies.put('bolinho', data.token)
-        window.location.href = '#!/login';
+        emailGet.get({ email: $scope.email }).$promise.then(function (data2) {
+          console.log(data2)
+          $cookies.put('admin', data2.admin)
+          $cookies.put('bolinho', data.token)
+          window.location.href = '#!/login';
+        })
       }
     })
   }
 
   $scope.removeCookie = function () {
     $cookies.remove('bolinho');
+    $cookies.remove('admin');
     window.location.href = '#!/';
   }
 });
